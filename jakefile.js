d9ee92a395bfd0cb13f8cfb22d9ef9840616d9eb
Matt Sell <mwsell@gmail.com>
2013-05-31 12:25:59 -0500

    supply lint options

--- jakefile.js
+++ jakefile.js
@@ -1,20 +1,35 @@
 /*global desc, task, jake, fail, complete */
-"use strict";
+(function() {
+	"use strict";
 
-// with jake we provide tasks that execute functions
-task("default",["lint"]);
+	// with jake we provide tasks that execute functions
+	task("default", ["lint"]);
 
-desc("Lint all the things");
-task("lint", [], function(){
-	var lint = require("./build/lint/lint_runner.js");
-	var files = new jake.FileList();
-	files.include("**/*.js");
-	files.exclude("node_modules");
-	files.exclude("build");
-	
-	var options = {
-		node: true
-	};
-	
-	lint.validateFileList(files.toArray(), options, {});
-});
+	desc("Lint all the things");
+	task("lint", [], function() {
+		var lint = require("./build/lint/lint_runner.js");
+		var files = new jake.FileList();
+		files.include("**/*.js");
+		files.exclude("node_modules");
+		//files.exclude("build");
+		var options = {
+			bitwise: true,
+			curly: false,
+			eqeqeq: true,
+			forin: true,
+			immed: true,
+			latedef: true,
+			newcap: true,
+			noarg: true,
+			noempty: true,
+			nonew: true,
+			regexp: true,
+			undef: true,
+			strict: true,
+			trailing: true,
+			node: true
+		};
+
+		lint.validateFileList(files.toArray(), options, {});
+	});
+})();
