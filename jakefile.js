ede1e3bb372a9c282f4284143ed2e55026e78227
Matt Sell <mwsell@gmail.com>
2013-05-30 23:42:22 -0500

    tell lint which files to lint

--- jakefile.js
+++ jakefile.js
@@ -1,8 +1,20 @@
+/*global desc, task, jake, fail, complete */
+"use strict";
+
 // with jake we provide tasks that execute functions
 task("default",["lint"]);
 
 desc("Lint all the things");
 task("lint", [], function(){
 	var lint = require("./build/lint/lint_runner.js");
-	lint.validateFile("jakefile.js", {}, {});
+	var files = new jake.FileList();
+	files.include("**/*.js");
+	files.exclude("node_modules");
+	files.exclude("build");
+	
+	var options = {
+		node: true
+	};
+	
+	lint.validateFileList(files.toArray(), options, {});
 });
